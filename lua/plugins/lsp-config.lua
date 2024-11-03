2e56cef5343188cf3b088c27c6d3b4609b2007ab
tyr74 <travis.c.ainsworth@gmail.com>
2024-11-03 14:42:17 +0000

    Finished debugging the linters and formatters

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,46 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "asm_lsp",
-          "clangd",
-          "harper_ls",
-          "gopls",
-          "marksman",
-          "matlab_ls",
-          "pylyzer",
-          "rust_analyzer",
-          "zls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"asm_lsp",
+					"clangd",
+					"harper_ls",
+					"gopls",
+					"marksman",
+					"matlab_ls",
+					"pylyzer",
+					"rust_analyzer",
+					"zls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.pylyzer.setup({})
-      lspconfig.ruff.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.asm_lsp.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.harper_ls.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.matlab_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.zls.setup({})
-    end,
-  },
+			lspconfig.pylyzer.setup({})
+			lspconfig.ruff.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.bacon_ls.setup({})
+			lspconfig.asm_lsp.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.harper_ls.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.matlab_ls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.zls.setup({})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,24 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+  },
   config = function()
     local null_ls = require("null-ls")
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.bacon,
-        null_ls.builtins.diagnostics.cpplint,
+        --null_ls.builtins.diagnostics.cpplint,
+        require('none-ls.diagnostics.cpplint'),
         null_ls.builtins.diagnostics.markdownlint,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.diagnostics.staticcheck,
         null_ls.builtins.diagnostics.trivy,
+        null_ls.builtins.diagnostics.selene,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.asmfmt,
-        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.gotests,
         null_ls.builtins.formatting.mdformat,
-        null_ls.builtins.formatting.ruff,
       },
     })
   end,
