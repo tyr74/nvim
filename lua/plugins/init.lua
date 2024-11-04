81ee98f2daf541b27a6fc205edccb91de7194c6f
tyr74 <travis.c.ainsworth@gmail.com>
2024-11-04 16:04:46 +0000

    Added hardtime and test groundwork

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,31 +1,37 @@
 return {
-  {
-    "nvim-tree/nvim-web-devicons",
-  },
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  {
-    "mbbill/undotree",
-  },
+	{
+		"mbbill/undotree",
+	},
 
-  {
-    "tpope/vim-commentary",
-  },
+	{
+		"tpope/vim-commentary",
+	},
 
-  {
-    "Wansmer/sibling-swap.nvim",
-    requires = { "nvim-treesitter" },
-    config = function()
-      require("sibling-swap").setup({ --[[ your config ]]
-      })
-    end,
-  },
+	{
+		"Wansmer/sibling-swap.nvim",
+		requires = { "nvim-treesitter" },
+		config = function()
+			require("sibling-swap").setup({ --[[ your config ]]
+			})
+		end,
+	},
 
-  {
-    "willothy/moveline.nvim",
-    build = "make",
-  },
+	{
+		"willothy/moveline.nvim",
+		build = "make",
+	},
 
-  {
-    "shortcuts/no-neck-pain.nvim",
-  },
+	{
+		"shortcuts/no-neck-pain.nvim",
+	},
+
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
 }
