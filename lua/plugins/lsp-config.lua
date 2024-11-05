60eefaec7214876c58de69f0609253d838c73609
tyr74 <travis.c.ainsworth@gmail.com>
2024-11-05 19:21:48 +0000

    Removed coq and worked with neoscroll

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,69 +15,67 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		"lukas-reineke/lsp-format.nvim",
-		"ms-jpq/coq_nvim",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local on_attach = require("lsp-format").on_attach
-			local coq = require("coq")
 			local lspconfig = require("lspconfig")
 
-			lspconfig.pylyzer.setup(coq.lsp_ensure_capabilities({
+			lspconfig.pylyzer.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.hls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.hls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.ruff.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.ruff.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.rust_analyzer.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.rust_analyzer.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.bacon_ls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.bacon_ls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.asm_lsp.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.asm_lsp.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.clangd.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.clangd.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.harper_ls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.harper_ls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.marksman.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.marksman.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.matlab_ls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.matlab_ls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.gopls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.gopls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.zls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.zls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
-			lspconfig.jdtls.setup(coq.lsp_ensure_capabilities({
+			})
+			lspconfig.jdtls.setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-			}))
+			})
 
 			vim.diagnostic.config({
 				virtual_text = {
@@ -87,36 +85,6 @@ return {
 					source = "always",
 				},
 			})
-		end,
-	},
-
-	{
-		"neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-		lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-		dependencies = {
-			-- main one
-			{ "ms-jpq/coq_nvim", branch = "coq" },
-
-			-- 9000+ Snippets
-			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-			-- Need to **configure separately**
-			{ "ms-jpq/coq.thirdparty", branch = "3p" },
-			-- - shell repl
-			-- - nvim lua api
-			-- - scientific calculator
-			-- - comment banner
-			-- - etc
-		},
-		init = function()
-			vim.g.coq_settings = {
-				auto_start = true, -- if you want to start COQ at startup
-				-- Your COQ settings here
-			}
-		end,
-		config = function()
-			-- Your LSP settings here
 		end,
 	},
 
